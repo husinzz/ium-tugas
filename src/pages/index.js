f2fede33740324ec47c69e4cefceb54503638228
husinzz <its.husin@gmail.com>
2021-12-30 11:53:00 +0700

    gg gaming

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Link} from "gatsby"
-import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Carousel from "../components/carousel";
 
